6af973c142e21b60e20c32082129bdc23f8421c7
MouadxBth <mouad.bouthaich@outlook.com>
2023-01-28 23:58:10 +0100

    ~Initial Setup
    Bootstrapped the project using create-next-app, configured eslint, tailwindcss and prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,10 @@
 module.exports = {
   content: [
     "./pages/**/*.{html,js,jsx,ts,tsx}",
-    "./components/**/*.{html,js,jsx,ts,tsx}"
+    "./components/**/*.{html,js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
